3fb2d6f6de8c2659ea5666b7cddf918873aa9f2c
herveheritier <herve.heritier@gmail.com>
2018-11-23 00:22:19 +0100

    new Test object

--- scripts/test.js
+++ scripts/test.js
@@ -1,92 +1,115 @@
-var testCount = 0
-var testFailed = 0
+/*********************************************
+ * 
+ *    Test object offering test facilities
+ * 
+ *********************************************/
 
-/****************************************************************
-* fonction de pilotage des tests
-* ev : string du code a évaluer
-* attempt : valeur résultat attendue 
-* validFunc (optionnelle) : fonction de validation
-* la fonction de validation est réalisée ssi attempt est à null
-*
-* si le test est ok, l'évaluation est rejouée 1000 fois 
-* pour mesurer les performances
-******************************************************************/ 
-function test(ev,attempt,validFunc=null) {
-    testCount++
-    console.group(ev)
-    var res = eval(ev)
-    //console.log(res)
-    var ok = false
-    if(validFunc!=null) {
-        var vf = validFunc(res)
-        if(Set.prototype.isPrototypeOf(res)) console.log([...res])
-        else if(Array.prototype.isPrototypeOf(res)) console.log(res)
-        else console.log(JSON.stringify(res,null,4))
-        if(!vf) testFailed++ 
-        else ok = true
-    }
-    else if(Array.prototype.isPrototypeOf(attempt) || Set.prototype.isPrototypeOf(attempt)) {
-        if(Set.prototype.isPrototypeOf(res)) {
-            console.log(Object.prototype.isPrototypeOf(res[0]) ? JSON.stringify(res,null,4) : [...res])
-        }
-        else if(Array.prototype.isPrototypeOf(res)) {
-            console.log(Object.prototype.isPrototypeOf(res[0]) ? JSON.stringify(res,null,4) : res)
-        }
-        else {
-            console.log(Object.prototype.isPrototypeOf(res) ? JSON.stringify(res,null,4) : res)
-        }
-        if(!Sets.areEqual(res,new Set(attempt))) testFailed++
-        else ok = true
-    }
-    else if(res!=attempt) console.log(res), testFailed++
-    else console.log(Object.prototype.isPrototypeOf(res) ? JSON.stringify(res,null,4) : res), ok = true
+const Test = {
 
-    if(ok) {
-        console.log("Test passed... ")
-        var t0 = performance.now()
-        for(var i =0;i<1000;i++) eval(ev)
-        var t1 = performance.now()
-        console.log((t1-t0).toFixed(2)+"ms elapsed for 1000 executions")
+    level : 1,
+    count : 0,
+    failed : 0,
+    repeat : 1000,
+    mute : false,
+    testComponent : [],
+    testTimer: null,
+
+    // reroutage des fonctions log, group et groupEnd de l'objet console
+
+    done : (function(){
+        var devToolsLog = console.log;
+        console.log = function (message) {
+            var pre = document.createElement('pre')
+            pre.appendChild(document.createTextNode(message))
+            document.querySelector("body").appendChild(pre)
+            pre.style.display = 'none'
+            pre.style.display = ''
+            if(!Test.mute) devToolsLog.apply(console, arguments)
+        };
+        var devToolsGroup = console.group
+        console.group = function (message) {
+            var h = document.createElement('h'+Test.level)
+            h.appendChild(document.createTextNode(message))
+            document.querySelector("body").appendChild(h)
+            if(!Test.mute) devToolsGroup.apply(console, arguments)
+            Test.level++
+        }
+        var devToolsEnd = console.groupEnd
+        console.groupEnd = function () {
+            Test.level--
+            if(!Test.mute) devToolsEnd.apply(console, arguments)
+        }
+    })(),
+
+    // desactivation / activation de la log de la console du devTools
+    // si inactive, le reroutage est exclusif
+    // si active, les messages console sont à la fois rerouté et émis dans la console
+    devToolsMute : () => Test.mute = true,
+    devToolsLoud : () => Test.mute = false,
+    setRepeat : (repeat) => Test.repeat = repeat,
+
+    /****************************************************************
+    * fonction de pilotage des tests
+    * ev : string du code a évaluer
+    * attempt : valeur résultat attendue 
+    * validFunc (optionnelle) : fonction de validation
+    * la fonction de validation est réalisée ssi attempt est à null
+    *
+    * si le test est ok, l'évaluation est rejouée 1000 fois 
+    * pour mesurer les performances
+    ******************************************************************/ 
+    run : (ev,attempt,validFunc=null) => {
+        Test.count++
+        console.group(ev)
+        //var res = eval(ev)
+        var res = ev()
+        //console.log(res)
+        var ok = false
+        if(validFunc!=null) {
+            var vf = validFunc(res)
+            if(Set.prototype.isPrototypeOf(res)) console.log([...res])
+            else if(Array.prototype.isPrototypeOf(res)) console.log(res)
+            else console.log(JSON.stringify(res,null,0))
+            if(!vf) Test.failed++ 
+            else ok = true
+        }
+        else if(Array.prototype.isPrototypeOf(attempt) || Set.prototype.isPrototypeOf(attempt)) {
+            if(Set.prototype.isPrototypeOf(res)) {
+                console.log(Object.prototype.isPrototypeOf(res[0]) ? JSON.stringify(res,null,0) : [...res])
+            }
+            else if(Array.prototype.isPrototypeOf(res)) {
+                console.log(Object.prototype.isPrototypeOf(res[0]) ? JSON.stringify(res,null,0) : res)
+            }
+            else {
+                console.log(Object.prototype.isPrototypeOf(res) ? JSON.stringify(res,null,0) : res)
+            }
+            if(!Sets.areEqual(res,new Set(attempt))) Test.failed++
+            else ok = true
+        }
+        else if(res!=attempt) console.log(res), Test.failed++
+        else console.log(Object.prototype.isPrototypeOf(res) ? JSON.stringify(res,null,0) : res), ok = true
+
+        if(ok) {
+            console.log("Test passed... ")
+            var t0 = performance.now()
+            for(var i =0;i<Test.repeat;i++) ev() //eval(ev)
+            var t1 = performance.now()
+            console.log((t1-t0).toFixed(2)+"ms elapsed for "+Test.repeat+" executions")
+        }
+        else console.log("Test failed... ")
+        
+        console.groupEnd()
+
+    },
+
+    // compte-rendu de test
+
+    results : () => {
+        console.group('****** Tests Report *******')
+        console.log("tests executed : "+Test.count)
+        console.log("tests passed   : "+(Test.count-Test.failed))
+        console.log("tests failed   : "+Test.failed)
+        console.groupEnd()
     }
-    else console.log("Test failed... ")
-    
-    console.groupEnd()
+
 }
-
-// compte-rendu de test
-
-function testResults() {
-    console.group('****** Tests Report *******')
-    console.log("tests executed : "+testCount)
-    console.log("tests passed   : "+(testCount-testFailed))
-    console.log("tests failed   : "+testFailed)
-    console.groupEnd()
-}
-
-// reroutage des fonctions log, group et groupEnd de l'objet console
-
-var level = 1;
-
-(function(){
-    var oldLog = console.log;
-    console.log = function (message) {
-        var pre = document.createElement('pre')
-        pre.appendChild(document.createTextNode(message))
-        document.querySelector("body").appendChild(pre)
-        //document.querySelector("body").appendChild(document.createElement('br'))
-        oldLog.apply(console, arguments);
-    };
-    var oldGroup = console.group
-    console.group = function (message) {
-        var h = document.createElement('h'+level)
-        h.appendChild(document.createTextNode(message))
-        document.querySelector("body").appendChild(h)
-        oldGroup.apply(console, arguments);
-        level++
-    }
-    var oldGroupEnd = console.groupEnd
-    console.groupEnd = function () {
-        level--
-        oldGroupEnd.apply(console, arguments)
-    }
-})();
